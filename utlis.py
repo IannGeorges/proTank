edd2ec45e5dd3a9bab923518b9757be1500a418c
IannGeorges <44213678+IannGeorges@users.noreply.github.com>
2021-05-05 09:39:19 -0500

    Update utlis.py

--- utlis.py
+++ utlis.py
@@ -69,7 +69,7 @@ def getHistogram(img,minPer=0.1,display=False, region=1):
     if display:
         imgHist = np.zeros((img.shape[0],img.shape[1],3),np.uint8)
         for x,intensity in enumerate(histValues):
-            cv2.line(imgHist,(x,img.shape[0]), (x,img.shape[0]-intensity),(255,0,255),1)
+            cv2.line(imgHist,(x,img.shape[0]), (x,int(img.shape[0]-intensity)),(255,0,255),1)
             cv2.circle(imgHist,(basePoint,img.shape[0]),20,(0,255,255),cv2.FILLED)
         return basePoint,imgHist
  
